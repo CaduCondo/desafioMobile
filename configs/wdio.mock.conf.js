5c0c1a34233fcae625a4de36fed2127ff8cfe9e5
Carlos Uva <stefcadu@gmail.com.br>
2026-01-12 21:20:18 -0300

    correcao chrome in headless mode

--- configs/wdio.mock.conf.js
+++ configs/wdio.mock.conf.js
@@ -1,16 +1,28 @@
-
 exports.config = {
   runner: 'local',
+
   specs: [
-  process.cwd() + '/test/specs/**/*.spec.js'
-],
+    process.cwd() + '/test/specs/**/*.spec.js'
+  ],
 
   maxInstances: 1,
+
   capabilities: [{
-    browserName: 'chrome'
+    browserName: 'chrome',
+    'goog:chromeOptions': {
+      args: [
+        '--headless=new',
+        '--disable-gpu',
+        '--no-sandbox',
+        '--disable-dev-shm-usage'
+      ]
+    }
   }],
+
   framework: 'mocha',
+
   reporters: ['spec'],
+
   mochaOpts: {
     timeout: 60000
   }
